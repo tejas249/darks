0d1b625c3bb50e90c7b9cbbc4a3c87592ef2fb12
Tejas Kamble <tejaskambletk132@gmail.com>
2025-11-27 15:47:28 +0530

    navbar responsiveness and buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,13 @@ import getAssetUrl from "../lib/getAssetUrl";
 
 export const Navbar = () => {
   return (
-    <div className="p-1 bg-black">
-      <div className="p-2 flex items-center justify-between gap-3">
+    <header className="p-1 bg-black self-center">
+      <div className="p-2 flex items-center justify-between">
 
         {/* Logo + Glow */}
-        <div className="relative w-12 h-12 flex items-center justify-center">
-          <div className="absolute inset-0 bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD98,#C2F0B1,#2FD8FE)] blur-lg rounded-full"></div>
+        <div className="relative w-12 h-12 flex items-center justify-center ml-10">
+          <div className="absolute inset-0 bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD98,#C2F0B1,#2FD8FE)] 
+            blur-lg rounded-full"></div>
 
           <Image
             src={logoImage}
@@ -21,16 +22,27 @@ export const Navbar = () => {
           />
         </div>
 
-        {/* Menu Icon */}
-        <div className="border border-gray-600/30 h-10 w-10 inline-flex justify-center items-center rounded-lg sm:hidden">
+        {/* Desktop Navigation */}
+        <nav className="hidden gap-6 items-center sm:flex ">
+          <a href="#about" className="text-white/60 hover:text-white transition">About</a>
+          <a href="#features" className="text-white/60 hover:text-white transition">Features</a>
+          <a href="#updates" className="text-white/60 hover:text-white transition">Updates</a>
+          <a href="#help" className="text-white/60 hover:text-white transition">Help</a>
+          <a href="#customers" className="text-white/60 hover:text-white transition">Customers</a>
+          <button className="bg-white py-2 px-4 rounded-lg">Get It Free</button>
+        </nav>
+
+        {/* Mobile Menu Button */}
+        <button className="sm:hidden border border-gray-600/30 h-10 w-10 flex justify-center items-center rounded-lg">
           <img
             src={getAssetUrl(menuIcon)}
             alt="menu"
             className="h-6 w-6"
             style={{ filter: "invert(1)" }}
           />
-        </div>
+        </button>
+
       </div>
-    </div>
+    </header>
   );
 };
